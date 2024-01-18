6456516427dc9ecd1be0a86bc7f63f118b028755
MartinIlg12 <martinilguan12221@gmail.com>
2024-01-18 17:20:52 -0500

    okDañado

--- clasificaciones/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClasificacionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clasificaciones'
-    verbose_name_plural="Clasificación de Autores y Poemas"
--- clasificaciones/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Clasificaciones
-
-# Create your views here.
-
-def about(request):
-    clasificaciones = Clasificaciones.objects.all()
-    return render(request,"clasificaciones/about.html",{'clasificaciones':clasificaciones})
